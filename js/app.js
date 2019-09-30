dc6a4e21e4f5992e8d427fd11cc949ac9b2e4359
haleywalsh <haleywalsh@users.noreply.github.com>
2019-09-30 13:24:06 -0400

    Added more content, started navigation menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 $(document).foundation()
+
+// hide the overlay initially
+$('.overlay').hide();
+
+// create a button to close the overlay
+// add it to the overlay
+$('.overlay').append('<button>X</button>');
+
+// open overlay
+$('main a').click(function(){
+  $('.overlay').show();
+});
+
+// close overlay
+$('.overlay button').click(function(){
+  $('.overlay').hide();
+});
+
+$('li a').click(function(){
+  $('.overlay').hide();
+});
