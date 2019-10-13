1a1a171489bedeedec85a2f61b69675c667d9ff9
haleywalsh <haleywalsh@users.noreply.github.com>
2019-10-13 18:56:16 -0400

    as done as it can be

--- js/app.js
+++ js/app.js
@@ -21,3 +21,20 @@ $('.overlay button').click(function(){
 $('li a').click(function(){
   $('.overlay').hide();
 });
+
+$('.menu a').click(function(event){
+  
+  // prevent defalt click behavior
+  // don't jump to content
+  event.preventDefault();
+    
+  // identify position of target
+  var target = $(this).attr('href');
+  var top = $(target).offset().top;
+
+  // animate scroll to target
+  $('html,body').animate({
+    scrollTop: top
+    },700);
+
+});
